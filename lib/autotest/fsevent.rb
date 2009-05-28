c4d0565bb79cca906c4143369cafcc98f0f5627c
Sven Schwyn <schwyn@bitcetera.com>
2009-05-28 13:09:02 +0200

    Fix GEM_PATH.

--- lib/autotest/fsevent.rb
+++ lib/autotest/fsevent.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'autotest'
 
 $:.unshift(File.dirname(__FILE__)) unless
@@ -14,14 +15,15 @@ $:.unshift(File.dirname(__FILE__)) unless
 #   require 'autotest/fsevent'
 module Autotest::FSEvent
 
-  VERSION = '0.1.0'
-  GEM_DIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  VERSION  = '0.1.0'
+  GEM_PATH = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
 
   ##
   # Use FSEvent if possible
   Autotest.add_hook :waiting do
     if RUBY_PLATFORM.match(/darwin(\d+)\.?(\d+)?/) && ($1.to_i >= 9 || ($1.to_i == 8 && $2.to_i >= 9))
-      `#{File.join(GEM_DIR, 'fsevent', 'fsevent_sleep')} '#{Dir.pwd}' 2>&1`
+      `#{File.join(GEM_PATH, 'fsevent', 'fsevent_sleep')} '#{Dir.pwd}' 2>&1`
+
     else
       puts
       puts "autotest-fsevent: #{RUBY_PLATFORM} not supported, please file a bug if you are on Mac OS X 10.5 or above."
