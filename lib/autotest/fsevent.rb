c3f6d63d07f26cf74d7548a838c04333e43ae4a2
lotusone <merchants08@clipperadams.com>
2010-01-18 04:46:36 +0800

    Autotest calls the waiting hook before going into the loop. Some plugins (like timestamp) depend on this behavior.
    This version has the following benefits:
    1. only performs platform check once
    2. Keeps :waiting hook outside the loop
    3. does nothing if not supported i.e. continues to use polling

--- lib/autotest/fsevent.rb
+++ lib/autotest/fsevent.rb
@@ -15,31 +15,28 @@ include Sys
 #   require 'autotest/fsevent'
 module Autotest::FSEvent
 
-  GEM_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
+	GEM_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 
-  ##
-  # Use FSEvent if possible
-  Autotest.add_hook :waiting do
-    if (Uname.sysname == 'Darwin' && Uname.release.to_i >= 9) || %w(Linux).include?(Uname.sysname)
-#     `#{File.join(GEM_PATH, 'fsevent', Uname.sysname.downcase, 'fsevent_sleep')} '#{Dir.pwd}' 2>&1`
-      `cd '#{Dir.pwd}'; #{File.join(GEM_PATH, 'fsevent', Uname.sysname.downcase, 'fsevent_sleep')} . 2>&1`
-    else
-      puts
-      puts "autotest-fsevent: platform #{Uname.sysname} #{Uname.release} is not supported"
-    end
-  end
+	##
+	# Use FSEvent if possible
 
-  ##
-  # Add waiting hook to prevent fallback to polling after ignored files have changed
-  Autotest.add_hook :initialize do
-    class ::Autotest
-      def wait_for_changes
-        begin
-          hook :waiting
-          Kernel.sleep self.sleep
-        end until find_files_to_test
-      end
-    end
-  end
-
+	##
+	# Add waiting hook to prevent fallback to polling after ignored files have changed
+	Autotest.add_hook :initialize do
+		if (Uname.sysname == 'Darwin' && Uname.release.to_i >= 9) || %w(Linux).include?(Uname.sysname)
+			class ::Autotest
+				def wait_for_changes
+					hook :waiting
+					begin
+						#     `#{File.join(GEM_PATH, 'fsevent', Uname.sysname.downcase, 'fsevent_sleep')} '#{Dir.pwd}' 2>&1`
+						`cd '#{Dir.pwd}'; #{File.join(GEM_PATH, 'fsevent', Uname.sysname.downcase, 'fsevent_sleep')} . 2>&1`
+						Kernel.sleep self.sleep
+					end until find_files_to_test
+				end
+			end
+		else
+			puts
+			puts "autotest-fsevent: platform #{Uname.sysname} #{Uname.release} is not supported"
+		end
+	end
 end
