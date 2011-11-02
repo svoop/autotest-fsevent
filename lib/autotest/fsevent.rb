0c7f2fd133a54352c4d200cd6bb39cfe265f8262
Sven Schwyn <schwyn@bitcetera.com>
2011-11-02 13:15:08 +0100

    Replace Dir.exists? with File.exists? to ensure Ruby 1.8.7 compatibility.

--- lib/autotest/fsevent.rb
+++ lib/autotest/fsevent.rb
@@ -15,27 +15,27 @@ include Sys
 #   require 'autotest/fsevent'
 module Autotest::FSEvent
 
-	GEM_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')) unless defined?(GEM_PATH)
+  GEM_PATH = File.expand_path(File.join(File.dirname(__FILE__), '..', '..')) unless defined?(GEM_PATH)
 
-	##
-	# Use FSEvent if possible 
-	# Add waiting hook to prevent fallback to polling after ignored files have changed
-	Autotest.add_hook :initialize do
-		if Uname.sysname == 'Darwin' && Uname.release.to_i >= 9
-			class ::Autotest
-			  remove_method :wait_for_changes
-				def wait_for_changes
-					hook :waiting
-					begin
-						`cd '#{Dir.pwd}'; #{File.join(GEM_PATH, 'bin', 'fsevent_sleep')} . 2>&1`
-						Kernel.sleep self.sleep
-					end until find_files_to_test
-				end
-			end
-		else
-			puts
-			puts "autotest-fsevent: platform #{Uname.sysname} #{Uname.release} is not supported"
-		end
-	end
+  ##
+  # Use FSEvent if possible 
+  # Add waiting hook to prevent fallback to polling after ignored files have changed
+  Autotest.add_hook :initialize do
+    if Uname.sysname == 'Darwin' && Uname.release.to_i >= 9
+      class ::Autotest
+        remove_method :wait_for_changes
+        def wait_for_changes
+          hook :waiting
+          begin
+            `cd '#{Dir.pwd}'; #{File.join(GEM_PATH, 'bin', 'fsevent_sleep')} . 2>&1`
+            Kernel.sleep self.sleep
+          end until find_files_to_test
+        end
+      end
+    else
+      puts
+      puts "autotest-fsevent: platform #{Uname.sysname} #{Uname.release} is not supported"
+    end
+  end
 
 end
