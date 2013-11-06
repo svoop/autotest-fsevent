f0a56d839461d4637af389ee87f375cb4a280e00
Sven Schwyn <schwyn@bitcetera.com>
2013-11-06 08:40:54 +0100

    Include Mac OS X 10.9 Mavericks.

--- ext/fsevent/extconf.rb
+++ ext/fsevent/extconf.rb
@@ -19,7 +19,7 @@ emulate_extension_install('fsevent')
 if `uname -s`.chomp == 'Darwin'
   GEM_ROOT       = File.expand_path(File.join('..', '..'))
   DARWIN_VERSION = `uname -r`.to_i
-  SDK_VERSION    = { 9 => '10.5', 10 => '10.6', 11 => '10.7', 12 => '10.8' }[DARWIN_VERSION]
+  SDK_VERSION    = { 9 => '10.5', 10 => '10.6', 11 => '10.7', 12 => '10.8', 13 => '10.9' }[DARWIN_VERSION]
   
   raise "Darwin #{DARWIN_VERSION} is not (yet) supported" unless SDK_VERSION
   
