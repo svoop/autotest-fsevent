c3861b8fef86dd21547aa12a17540964f6fa3e1e
Sven Schwyn <sven.schwyn@bitcetera.com>
2023-12-08 15:26:32 +0100

    Remove now obsolete Rake task

--- rakefile.rb
+++ rakefile.rb
@@ -9,16 +9,4 @@ Rake::TestTask.new do |t|
   t.warning = true
 end
 
-namespace :build do
-  desc "Build checksums of all gems in pkg directory"
-  task :checksum do
-    require 'digest/sha2'
-    Dir.mkdir('checksum') unless Dir.exist?('checksum')
-    Dir.glob('*.gem', base: 'pkg').each do |gem|
-      checksum = Digest::SHA512.new.hexdigest(File.read("pkg/#{gem}"))
-      File.open("checksum/#{gem}.sha512", 'w') { |f| f.write(checksum) }
-    end
-  end
-end
-
 task default: :test
